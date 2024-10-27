7946d143f0429dfd88ed70804f37c70edeb94cbb
pranulkbv28 <28bvpranaav@gmail.com>
2024-10-27 15:42:57 +0530

    project creation

---
+++ Models/RecordContext.cs
@@ -0,0 +1,18 @@
+﻿using MvcEntityDemo.Models;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+namespace MvcEntityDemo.Models
+{
+    public class RecordContext : DbContext
+    {
+        public RecordContext() : base("RecordContext")
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcEntityDemo.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Course is required")]
+        public string Course { get; set; }
+        [Required(ErrorMessage = "Contact Information is required")]
+        //[DataType(DataType.PhoneNumber)]
+        //[RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid Phone number")]
+        public string Contact { get; set; }
+    }
+}
